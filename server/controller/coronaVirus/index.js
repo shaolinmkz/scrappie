ecaf9f5dd35988f8fc3def00603c16306ca47c69
shaolinmkz007 <emeka@softcom.ng>
2020-04-17 01:21:50 +0100

    add cron job

--- server/controller/coronaVirus/index.js
+++ server/controller/coronaVirus/index.js
@@ -4,6 +4,77 @@ const {
 const cheerio = require('cheerio');
 const fs = require('fs');
 
+export const scrapWorldMeter = ({ req, res, sent }) => {
+  const worldoMeters = 'https://www.worldometers.info/coronavirus/';
+  const savePath = 'server/controller/coronaVirus/covid19-world-stats.json';
+
+  get(worldoMeters)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+
+      const worldStat = $('#main_table_countries_today > tbody > tr').map(function () {
+        const country = `${$(this).find('td:nth-child(1)').text()}`.trim() ||
+        `${$(this).find('td:nth-child(1) > a').text()}`.trim() ||
+        `${$(this).find('td:nth-child(1) > span').text()}`.trim();
+        const totalCases = `${$(this).find('td:nth-child(2)').text()}`.trim();
+        const newCases = `${$(this).find('td:nth-child(3)').text()}`.trim();
+        const totalDeaths = `${$(this).find('td:nth-child(4)').text()}`.trim();
+        const newDeaths = `${$(this).find('td:nth-child(5)').text()}`.trim();
+        const totalRecovery = `${$(this).find('td:nth-child(6)').text()}`.trim();
+        const activeCases = `${$(this).find('td:nth-child(7)').text()}`.trim();
+        const seriousCritical = `${$(this).find('td:nth-child(8)').text()}`.trim();
+        const totalCasesPer1mPop = `${$(this).find('td:nth-child(9)').text()}`.trim();
+        const deathsPer1mPop = `${$(this).find('td:nth-child(10)').text()}`.trim();
+        const totalTests = `${$(this).find('td:nth-child(11)').text()}`.trim();
+        const testsPer1mPop = `${$(this).find('td:nth-child(12)').text()}`.trim();
+
+        return {
+          country,
+          totalCases,
+          newCases,
+          totalDeaths,
+          newDeaths,
+          totalRecovery,
+          activeCases,
+          seriousCritical,
+          totalCasesPer1mPop,
+          deathsPer1mPop,
+          totalTests,
+          testsPer1mPop,
+        }
+      }).toArray();
+
+      const worldTotal = worldStat.pop();
+
+      let data = {
+        worldTotal,
+        worldStats: worldStat
+        .filter(({ country }) => country && country.toLowerCase() !== 'total:')
+        .sort((dataA, dataB) => dataA?.country.toUpperCase() > dataB?.country.toUpperCase() ? 1 : -1),
+      }
+
+      // write to file
+      fs.writeFileSync(savePath, JSON.stringify(data, null, 2));
+
+
+      if (!sent && res) {
+        res.status(200).json({
+          status: 200,
+          method: req.method,
+          message: 'successfully retrieved world stats for covid-19',
+          data
+        });
+      }
+    })
+    .catch(error => {
+      if(res) res.status(500).json({
+        status: 500,
+        method: req.method,
+        error: error?.message,
+      });
+    });
+}
+
 const getWorldStatsOnCovid = (req, res) => {
 
   try {
@@ -24,77 +95,15 @@ const getWorldStatsOnCovid = (req, res) => {
       });
     }
 
-    const worldoMeters = 'https://www.worldometers.info/coronavirus/';
-    get(worldoMeters)
-      .then((response) => {
-        const $ = cheerio.load(response.data);
+    scrapWorldMeter({req, res, sent});
 
-        const worldStat = $('#main_table_countries_today > tbody > tr').map(function () {
-          const country = `${$(this).find('td:nth-child(1)').text()}`.trim() ||
-          `${$(this).find('td:nth-child(1) > a').text()}`.trim() ||
-          `${$(this).find('td:nth-child(1) > span').text()}`.trim();
-          const totalCases = `${$(this).find('td:nth-child(2)').text()}`.trim();
-          const newCases = `${$(this).find('td:nth-child(3)').text()}`.trim();
-          const totalDeaths = `${$(this).find('td:nth-child(4)').text()}`.trim();
-          const newDeaths = `${$(this).find('td:nth-child(5)').text()}`.trim();
-          const totalRecovery = `${$(this).find('td:nth-child(6)').text()}`.trim();
-          const activeCases = `${$(this).find('td:nth-child(7)').text()}`.trim();
-          const seriousCritical = `${$(this).find('td:nth-child(8)').text()}`.trim();
-          const totalCasesPer1mPop = `${$(this).find('td:nth-child(9)').text()}`.trim();
-          const deathsPer1mPop = `${$(this).find('td:nth-child(10)').text()}`.trim();
-          const totalTests = `${$(this).find('td:nth-child(11)').text()}`.trim();
-          const testsPer1mPop = `${$(this).find('td:nth-child(12)').text()}`.trim();
-
-          return {
-            country,
-            totalCases,
-            newCases,
-            totalDeaths,
-            newDeaths,
-            totalRecovery,
-            activeCases,
-            seriousCritical,
-            totalCasesPer1mPop,
-            deathsPer1mPop,
-            totalTests,
-            testsPer1mPop,
-          }
-        }).toArray();
-
-        const worldTotal = worldStat.pop();
-
-        let data = {
-          worldTotal,
-          worldStats: worldStat
-          .filter(({ country }) => country && country.toLowerCase() !== 'total:')
-          .sort((dataA, dataB) => dataA?.country.toUpperCase() > dataB?.country.toUpperCase() ? 1 : -1),
-        }
-
-        // write to file
-        fs.writeFileSync(savePath, JSON.stringify(data, null, 2));
-
-
-        if (!sent) {
-          res.status(200).json({
-            status: 200,
-            message: 'successfully retrieved world stats for covid-19',
-            data
-          });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({
-          status: 500,
-          error: error?.response,
-        });
-      });
   } catch (err) {
     res.status(500).json({
       status: 500,
       error: err,
     });
   }
-}
+};
 
 module.exports = {
   getWorldStatsOnCovid
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,13 @@ const {
 const softcomRoute = require('./softcomRoutes');
 const ncdcRoute = require('./ncdcRoutes');
 const coronaVirusRoute = require('./coronaVirusRoutes');
+const cronJobRouter = require('./cronJobRoutes');
 
 const router = Router();
 
 router.use(softcomRoute);
 router.use(ncdcRoute);
 router.use(coronaVirusRoute);
+router.use(cronJobRouter);
 
 module.exports = router;
